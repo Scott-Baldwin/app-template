10d1dfbec9574522db05e2d63cf5ebe7bd562d59
Scott-Baldwin <superscottyb@gmail.com>
2024-02-25 15:47:52 -0700

    adds icons to demo and browse windows

--- src/browse.py
+++ src/browse.py
@@ -1,10 +1,16 @@
+import os
 import tkinter as tk
 from tkinter import filedialog
+
+app_path = os.path.dirname(os.path.abspath(__file__))
+icon_path = os.path.join(app_path, "data", "file.ico")
 
 
 def file_browse(multiple=False, **args):
     root = tk.Tk()
     root.withdraw()
+    root.iconbitmap(icon_path)
+
     if multiple:
         path = filedialog.askopenfilenames(**args)
     else:
@@ -16,6 +22,8 @@ def file_browse(multiple=False, **args):
 def folder_browse(**args):
     root = tk.Tk()
     root.withdraw()
+    root.iconbitmap(icon_path)
+
     path = filedialog.askdirectory(**args)
     root.destroy()
     return path if path != "" else None
@@ -23,7 +31,7 @@ def folder_browse(**args):
 
 if __name__ == "__main__":
     # tests
-    filetypes = [("Fit File", "*fit.csv"), ("All Files", "*")]
+    # filetypes = [("Fit File", "*fit.csv"), ("All Files", "*")]
     # path = file_browse(filetypes=filetypes)
     # path = file_browse(multiple=True, filetypes=filetypes)
     path = folder_browse()
--- src/dpg_demo.py
+++ src/dpg_demo.py
@@ -1,10 +1,16 @@
+import os
+
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
 
 
+app_path = os.path.dirname(os.path.abspath(__file__))
+icon = os.path.join(app_path, "data", "rocket.ico")
+
+
 def main():
     dpg.create_context()
-    dpg.create_viewport(title="DPG Demo", width=650, height=450)
+    dpg.create_viewport(title="DPG Demo", width=650, height=450, large_icon=icon)
 
     demo.show_demo()
 
